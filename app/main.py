0cca1ae1fb7e16e47f5337a08e5016fba0e3e72b
Irish K Sajan <irishk.sajan@tigeranalytics.com>
2025-09-25 19:25:17 +0530

    Update main.py
    added cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],    # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],    # Allows all headers
+)
 
 @app.get("/")
 def root():
